cbc89e728463dfc9c99c8988a1ccd8a69e753579
Raghu Sidharthan <srprasad@utexas.edu>
2017-04-21 11:19:09 -0700

    Batch writes the Cube script to export to shape files. Need to specify whether both node and link shapefile is needed.

--- NetworkToShape.s
+++ NetworkToShape.s
@@ -1,5 +1,5 @@
-
-RUN PGM=NETWORK
-FILEI NETI="C:\Temp\Research\SampleCubeNet\S7_10_20170402_2.net"
-FILEO LINKO ="C:\Temp\Research\SampleCubeNet\S7_10_20170402_2.shp" FORMAT=SHP
-ENDRUN
+RUN PGM=NETWORK                                                  
+FILEI NETI ='C:\Temp\Research\SampleCubeNet\S7_10_REV_10132015 - EW Study.net'                                        
+FILEO LINKO='C:\Temp\Research\SampleCubeNet\S7_10_REV_10132015 - EW Study_link.shp' FORMAT=SHP  
+FILEO NODEO='C:\Temp\Research\SampleCubeNet\S7_10_REV_10132015 - EW Study_node.shp' FORMAT=SHP  
+ENDRUN                                                           
